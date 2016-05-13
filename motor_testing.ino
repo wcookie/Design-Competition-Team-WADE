48dc62a4625f9659edbd7c4bba4257fed4d1f240
Wyatt Cook <wyattcook2018@u.northwestern.edu>
2016-05-13 03:04:32 -0500

    Started coordination of code with all of the moving parts.  Logic is not written yet, however the basic variables have been created

--- motor_testing.ino
+++ motor_testing.ino
@@ -1,5 +1,5 @@
   #include<Servo.h> //including the servo library
-  
+  //////////////////////////////////////
   Servo laser_servo; //initializing the servo for the laser
   //////////////////////////////////////
   //these 4 pins relate to the h bridges for the drive motors
@@ -8,10 +8,31 @@
   int analogRight = 3; //PWM out
   int digitalRight = 2; //Digital out
   //////////////////////////////////////
-  int servo_pin = 6; //PWM out
-  int prOneIn = A1 ; //Analog in for the first phototransistor
-  int prOneValue = 0; //analog value of the first phototransistor
+  //servo stuff
   int servo_pos = 0; // the servo position for the lasers
+  int servo_pin = 6; //PWM out
+  //////////////////////////////////////
+  //Pins for edge detection
+  int frontLeftLine = A3; //Analog in
+  int frontRightLine = A4; //Analog in
+  int backLine = A5; //Analog in
+  //////////////////////////////////////
+  //laser values
+  //left
+  int leftLaserPin = A1 ; //Analog in for the left phototransistor
+  int leftLaserValue = 0; //Analog value of the left phototransistor
+  //right
+  int rightLaserPin = A2; //Analog in for the right phototransistor
+  int rightLaserValue = 0; //Analog value of the right phototransistor
+  //////////////////////////////////////
+  //logic values
+  short detectionSide = 1; //detectionSide can be either 0, 1, or 2.  
+  //If it is 0, then we have last seen the cup on the left.  
+  //if it is 1 we have last seen it in the middle
+  //If it is 2 then we have last seen it on the right
+  bool isDetected = 0; //isDetected sees if we are currently looking at the cup
+  //
+  
 void setup() {
   // put your setup code here, to run once:
   //initializing pin modes
@@ -21,7 +42,7 @@ void setup() {
   pinMode(digitalRight, OUTPUT);
 
   pinMode(prOneIn, INPUT);
-  laser_servo.attach(servo_pin); //attaching the servo
+  //laser_servo.attach(servo_pin); //attaching the servo
   Serial.begin(9600); //allowing serial communication 
 }
 
@@ -43,11 +64,8 @@ void loop() {
   Serial.println(prOneValue);
 
   //moving the servo
-  servo_pos = 10;
+  servo_pos ++;
   laser_servo.write(servo_pos); //telling the servo to move to 100 degrees
-  delay(200000);
-  
+  delay(10);
 
-
-  
 }
